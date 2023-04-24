d91aaa6047b27dc488a3a9e876e971816b3ab467
Wudson Melo <wudson@sistemasbr.net>
2023-04-23 21:33:37 -0300

    criando arquivo de temas do app e estrutura inicial

---
+++ src/theme/index.ts
@@ -0,0 +1,33 @@
+export default {
+    COLORS: {
+      WHITE: '#FFFFFF',
+  
+      GREEN_DARK: '#639339',
+      GREEN_MID: '#CBE4B4',
+      GREEN_LIGHT: '#E5F0DB',
+  
+      RED_LIGHT: '#F4E6E7',
+      RED_MID: '#F3BABD',
+      RED_DARK: '#BF3B44',
+  
+      GRAY_700: '#FAFAFA',
+      GRAY_600: '#EFF0F0',
+      GRAY_500: '#DDDEDF',
+      GRAY_400: '#B9BBBC',
+      GRAY_300: '#5C6265',
+      GRAY_200: '#333638',
+      GRAY_100: '#1B1D1E'
+    },
+    FONT_FAMILY: {
+      REGULAR: 'Nunito_400Regular',
+      BOLD: 'Nunito_700Bold'
+    },
+    FONT_SIZE: {
+      TITLE_32: 32,
+      TITLE_24: 24,
+      TITLE_18: 18,
+      TITLE_16: 16,
+      BODY_14: 14,
+      BODY_12: 12
+    }
+  };
